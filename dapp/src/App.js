3f97f9398c5d69369aac63d2681117a7d6248957
Ilan Olkies <ilanolkies@outlook.com>
2020-03-22 17:07:37 -0300

    Get message

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: null,
+    };
+
+    this.getMessage = this.getMessage.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMessage();
+  }
+
+  getMessage() {
+    this.setState({ message: null });
+
+    const web3 = new Web3('http://localhost:9545');
+
+    const helloWorld =new web3.eth.Contract([
+      {
+        constant: true,
+        inputs: [],
+        name: "getMessage",
+        outputs: [
+          { internalType: "string", name: "", type: "string" }
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ], '0x138a4c489A657BA9419f21C2E0c4d9B265a67341');
+
+    helloWorld.methods.getMessage().call().then(message => this.setState({ message }));
+  }
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <Container style={{ textAlign: 'center' }}>
+        <Row>
+          <Col>
+            <h1>Hello world - RSK</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Message: {message || <Spinner animation="border" />}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default App;
